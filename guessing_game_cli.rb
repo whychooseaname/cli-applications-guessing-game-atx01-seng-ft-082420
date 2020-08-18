a2ffe39d1b0f7d2a208e0ad202eef2332c2e1495
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 04:09:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   number = rand 1..6
   puts "Guess a number"
   guess = gets.chomp 
-  
+  binding.pry 
   if guess == number
     puts "You guessed the correct number!"
   else puts "Sorry! The computer guessed #{number}"
