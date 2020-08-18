45aafcbe336f9c09e270665a35a042c06bea1b3d
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 04:47:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -11,7 +11,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif guess != number
     puts "Sorry! The computer guessed #{number}."
-  else guess == "exit"
+  elsif guess == "exit"
     puts "Goodbye!"
   else 
     puts "Please enter valid guess"
