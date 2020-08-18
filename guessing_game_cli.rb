7c6e821677bd1129ce4534d084de0a03ef3cb945
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-18 04:55:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,8 +4,8 @@ require 'pry'
 def run_guessing_game
   number = rand(6) + 1
   puts "Guess a number."
-  guess = gets.chomp
-  if guess == number.to_s
+  guess = gets.chomp.to_i
+  if guess == number
     puts "You guessed the correct number!"
   elsif guess == "exit"
     puts "Goodbye!"
